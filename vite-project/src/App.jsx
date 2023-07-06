60d2ef5cf1b1a7e82c3ff4d09d6122b50124ca76
hassan shahid <hassanshahid234@gmail.com>
2023-07-06 18:45:24 +0500

    hassan

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Components/Screens/Home/Home'
+import Contact from "./Components/Screens/Contact/Contact";
 
 function App() {
   return (
     <>
      <h1><Home/></h1>
+     <Contact/>
     </>
   );
 }
