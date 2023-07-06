d47cb926b394c80ff30f69508eee75b4372490db
hassan shahid <hassanshahid234@gmail.com>
2023-07-06 21:48:01 +0500

    firebase added

---
+++ vite-project/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAidodu1FF11cFwYaVN-QuPa8AcEaegEB8",
+  authDomain: "rcai-40ffd.firebaseapp.com",
+  projectId: "rcai-40ffd",
+  storageBucket: "rcai-40ffd.appspot.com",
+  messagingSenderId: "787814488676",
+  appId: "1:787814488676:web:096c9684201d42b7aa5d71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
