086c983b3ac193ca5fd34774f818ae80c2c3ef78
hassan shahid <hassanshahid234@gmail.com>
2023-07-06 18:52:00 +0500

    routes added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { useState } from 'react'
 import './App.css'
 import Home from './Components/Screens/Home/Home'
 import Contact from "./Components/Screens/Contact/Contact";
+import { Route, Routes } from "react-router-dom";
+import About from "./Components/Screens/About/About";
 
 function App() {
   return (
     <>
-     <h1><Home/></h1>
-     <Contact/>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
