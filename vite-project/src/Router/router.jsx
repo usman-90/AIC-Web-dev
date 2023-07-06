8ca482d25b14351d4b96a01e18ef085750f19bf7
Talal <talalzafar84@gmail.com>
2023-07-07 00:47:20 +0500

    adding event page

--- vite-project/src/Router/router.jsx
+++ vite-project/src/Router/router.jsx
@@ -1,32 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "../Components/Screens/Home/Home";
 import Contact from "../Components/Screens/Contact/Contact";
-import { Route, Routes } from "react-router-dom";
 import About from "../Components/Screens/About/About";
-<<<<<<< HEAD
 import Events from '../Components/Screens/Events/Events';
-=======
 import Login from "../Components/Screens/admin/Login";
 import Reset from "../Components/Screens/admin/Reset";
 import AdminPage from "../Components/Screens/admin/adminpg";
->>>>>>> aaf608ee7b4b99bcf530a57edd331ba33d9cf5be
 
 const Router = () => {
   return (
     <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/events" element={<Events />} />
-=======
-      <Route path="/admin" element={<AdminPage/>}/>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/reset" element={<Reset />} />
->>>>>>> aaf608ee7b4b99bcf530a57edd331ba33d9cf5be
+        <Route path="/admin" element={<AdminPage/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset" element={<Reset />} />s
     </Routes>
   );
 };
