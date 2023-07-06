1ac28e364e31f7884f2843ad81656ec17d6330fe
Talal <talalzafar84@gmail.com>
2023-07-06 15:27:10 +0500

    adding home page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Components/Screens/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>by hassan</h1>
+     <h1><Home/></h1>
     </>
   )
 }
